6a99224f7aa0964cb0f56fec836efe78bb8d8b7b
이백행 <dlqorgod@naver.com>
2023-06-19 08:22:17 +0900

    요청 메시지 구조

--- god.com-all/src/main/java/god/admin/cmm/service/GodRequestMessageDTO.java
+++ god.com-all/src/main/java/god/admin/cmm/service/GodRequestMessageDTO.java
@@ -1,5 +1,7 @@
 package god.admin.cmm.service;
 
+import org.apache.poi.ss.formula.functions.T;
+
 import lombok.ToString;
 
 /**
@@ -16,6 +18,16 @@ public class GodRequestMessageDTO {
      * 공유서비스 공통 메시지헤더
      */
     private ComMsgHeader comMsgHeader = new ComMsgHeader();
+
+    /**
+     * 제공 기관별 공통 메시지헤더
+     */
+    private MsgHeader msgHeader = new MsgHeader();
+
+    /**
+     * 서비스별 개별 메시지바디
+     */
+    private T msgBody;
 
     /**
      * 공유서비스 공통 메시지헤더 값읽기
@@ -36,6 +48,42 @@ public class GodRequestMessageDTO {
     }
 
     /**
+     * 제공 기관별 공통 메시지헤더 값읽기
+     * 
+     * @return
+     */
+    public MsgHeader getMsgHeader() {
+        return msgHeader;
+    }
+
+    /**
+     * 제공 기관별 공통 메시지헤더 값설정
+     * 
+     * @param msgHeader
+     */
+    public void setMsgHeader(MsgHeader msgHeader) {
+        this.msgHeader = msgHeader;
+    }
+
+    /**
+     * 서비스별 개별 메시지바디 값읽기
+     * 
+     * @return
+     */
+    public T getMsgBody() {
+        return msgBody;
+    }
+
+    /**
+     * 서비스별 개별 메시지바디 값설정
+     * 
+     * @param msgBody
+     */
+    public void setMsgBody(T msgBody) {
+        this.msgBody = msgBody;
+    }
+
+    /**
      * 공유서비스 공통 메시지헤더
      * 
      * @author 이백행
@@ -45,27 +93,78 @@ public class GodRequestMessageDTO {
     public class ComMsgHeader {
 
         /**
+         * 서비스 사용자별 서비스통합인증키
+         */
+        private String serviceKey;
+
+        /**
+         * 메시지요청일시
+         */
+        private String requestTime;
+
+        /**
+         * CallBack서비스URI
+         */
+        private String callBackURI;
+
+        /**
          * 요청 메시지 ID
          */
         private String requestMsgID;
 
-//        /**
-//         * 메시지 응답 일시
-//         */
-////    private LocalDateTime responseTime;
-//        private String responseTime;
-//
-//        /**
-//         * 응답 메시지 ID
-//         */
-//        private String responseMsgID;
-//
-//        /**
-//         * 처리 성공 여부
-//         */
-//        private SuccessYN successYN;
-//
-//        private String successYNName;
+        /**
+         * 서비스 사용자별 서비스통합인증키 값읽기
+         * 
+         * @return
+         */
+        public String getServiceKey() {
+            return serviceKey;
+        }
+
+        /**
+         * 서비스 사용자별 서비스통합인증키 값설정
+         * 
+         * @param serviceKey
+         */
+        public void setServiceKey(String serviceKey) {
+            this.serviceKey = serviceKey;
+        }
+
+        /**
+         * 메시지요청일시 값읽기
+         * 
+         * @return
+         */
+        public String getRequestTime() {
+            return requestTime;
+        }
+
+        /**
+         * 메시지요청일시 값설정
+         * 
+         * @param requestTime
+         */
+        public void setRequestTime(String requestTime) {
+            this.requestTime = requestTime;
+        }
+
+        /**
+         * CallBack서비스URI 값읽기
+         * 
+         * @return
+         */
+        public String getCallBackURI() {
+            return callBackURI;
+        }
+
+        /**
+         * CallBack서비스URI 값설정
+         * 
+         * @param callBackURI
+         */
+        public void setCallBackURI(String callBackURI) {
+            this.callBackURI = callBackURI;
+        }
 
         /**
          * 요청 메시지 ID 값읽기
@@ -85,73 +184,16 @@ public class GodRequestMessageDTO {
             this.requestMsgID = requestMsgID;
         }
 
-//        /**
-//         * 메시지 응답 일시 값읽기
-//         * 
-//         * @return
-//         */
-////    public LocalDateTime getResponseTime() {
-//        public String getResponseTime() {
-//            return responseTime;
-//        }
-//
-//        /**
-//         * 메시지 응답 일시 값설정
-//         * 
-//         * @param responseTime
-//         */
-////    public void setResponseTime(LocalDateTime responseTime) {
-//        public void setResponseTime(String responseTime) {
-//            this.responseTime = responseTime;
-//        }
-//
-//        /**
-//         * 응답 메시지 ID 값읽기
-//         * 
-//         * @return
-//         */
-//        public String getResponseMsgID() {
-//            return responseMsgID;
-//        }
-//
-//        /**
-//         * 응답 메시지 ID 값설정
-//         * 
-//         * @param responseMsgID
-//         */
-//        public void setResponseMsgID(String responseMsgID) {
-//            this.responseMsgID = responseMsgID;
-//        }
-//
-//        public SuccessYN getSuccessYN() {
-//            return successYN;
-//        }
-//
-//        public void setSuccessYN(SuccessYN successYN) {
-//            this.successYN = successYN;
-//            this.successYNName = successYN.getString();
-//        }
-//
-//        public String getSuccessYNName() {
-//            return successYNName;
-//        }
-
     }
 
-//    public enum SuccessYN {
-//
-//        Y("Sucess"), N("Fail");
-//
-//        private String name;
-//
-//        SuccessYN(String name) {
-//            this.name = name;
-//        }
-//
-//        public String getString() {
-//            return name;
-//        }
-//
-//    }
+    /**
+     * 제공 기관별 공통 메시지헤더
+     * 
+     * @author 이백행
+     *
+     */
+    static class MsgHeader {
+
+    }
 
 }
