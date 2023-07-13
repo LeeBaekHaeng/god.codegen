d44ada6a6814a99f36ced48378a019d2743353e6
이백행 <dlqorgod@naver.com>
2023-07-13 13:16:55 +0900

    no message

--- god.com-all/src/main/webapp/WEB-INF/jsp/god/admin/cmm/AdminCmmMain.jsp
+++ god.com-all/src/main/webapp/WEB-INF/jsp/god/admin/cmm/AdminCmmMain.jsp
@@ -14,47 +14,10 @@
 <br>${loginVO.uniqId}
 <c:out value="${loginVO.uniqId}" />
 
+<h1>test $.ajax</h1>
 <div id="test"></div>
 
+<h1>test axios</h1>
 <div id="test2"></div>
 
-<script type="text/javascript">
-    $(function() {
-        var data = {
-            codeId : 'COM001',
-        };
-        var settings = {};
-        settings.data = data;
-        settings.success = fn_success;
-        settings.error = fn_error;
-        fn_comCmm_ajaxGet('/rest/com/cmm/v1/cmmcodedetail', settings);
-        function fn_success(data, textStatus, jqXHR) {
-            $('#test').html(JSON.stringify(data));
-        }
-        function fn_error(jqXHR, textStatus, errorThrown) {
-            $('#test').html(textStatus);
-        }
-    });
-
-    $(function() {
-        var config = {};
-        config.url = '/rest/com/cmm/v1/cmmcodedetail-';
-        config.method = 'get';
-        config.params = {};
-        config.params.codeId = 'COM001';
-        fn_comCmm_axios(config).then(function(response) {
-            // 성공 핸들링
-            console.log(response);
-            $('#test2').html(JSON.stringify(response.data));
-        })
-  .catch(function (error) {
-    // 에러 핸들링
-          $('#test2').html(error.toJSON());
-  })
-  .finally(function () {
-    // 항상 실행되는 영역
-      console.log('항상 실행되는 영역2');
-  });
-
-    });
-</script>
+<script src="${pageContext.request.contextPath}/resources/admin/cmm/AdminCmmMain.js"></script>
---
+++ god.com-all/src/main/webapp/resources/admin/cmm/AdminCmmMain.js
@@ -0,0 +1,66 @@
+$(function() {
+
+    fn_adminCmm_getCode();
+    fn_adminCmm_getCode2();
+
+});
+
+/**
+ * jquery 코드 가져오기
+ */
+function fn_adminCmm_getCode() {
+    var settings = {};
+    settings.url = '/rest/com/cmm/v1/cmmcodedetail';
+
+    settings.method = 'GET';
+
+    settings.data = {};
+    settings.data.codeId = 'COM001';
+
+    settings.success = fn_success;
+    settings.error = fn_error;
+
+    fn_comCmm_ajax(settings);
+
+    function fn_success(data, textStatus, jqXHR) {
+        console.log('data', data);
+        console.log('textStatus', textStatus);
+        console.log('jqXHR', jqXHR);
+
+        $('#test').html(JSON.stringify(data));
+    }
+
+    function fn_error(jqXHR, textStatus, errorThrown) {
+        console.log('jqXHR', jqXHR);
+        console.log('textStatus', textStatus);
+        console.log('errorThrown', errorThrown);
+
+        $('#test').html(textStatus);
+    }
+}
+
+/**
+ * axios 코드 가져오기
+ */
+function fn_adminCmm_getCode2() {
+    var config = {};
+    config.url = '/rest/com/cmm/v1/cmmcodedetail';
+    config.method = 'get';
+    config.params = {};
+    config.params.codeId = 'COM001';
+
+    fn_comCmm_axios(config)
+        .then(function(response) {
+            // 성공 핸들링
+            console.log(response);
+            $('#test2').html(JSON.stringify(response.data));
+        })
+        .catch(function(error) {
+            // 에러 핸들링
+            $('#test2').html(error.toJSON());
+        })
+        .finally(function() {
+            // 항상 실행되는 영역
+            console.log('항상 실행되는 영역2');
+        });
+}
