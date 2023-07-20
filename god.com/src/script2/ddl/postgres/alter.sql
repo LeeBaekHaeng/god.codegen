944915e01fd01001422aa07695fe7da7665e9ee4
이백행 <dlqorgod@naver.com>
2023-07-20 13:17:57 +0900

    no message

--- god.com/src/script2/ddl/postgres/alter.sql
+++ god.com/src/script2/ddl/postgres/alter.sql
@@ -1,2 +1,5 @@
+ALTER TABLE comtccmmnclcode ALTER COLUMN cl_code TYPE varchar(30);
+ALTER TABLE comtccmmncode ALTER COLUMN cl_code TYPE varchar(30);
+
 ALTER TABLE comtccmmncode ALTER COLUMN code_id TYPE varchar(60);
 ALTER TABLE comtccmmndetailcode ALTER COLUMN code_id TYPE varchar(60);
--- god.com/src/script2/dic/postgres/컬럼.sql
+++ god.com/src/script2/dic/postgres/컬럼.sql
@@ -7,7 +7,8 @@ select
 from information_schema.columns a
 where 1 = 1
     and a.table_schema = 'public'
-    and a.column_name = 'code_id'
+--    and a.column_name = 'code_id'
+    and a.column_name = 'cl_code'
 order by
     a.table_catalog, a.table_schema, a.table_name, ordinal_position
 ;
