3bab3b608b345a06e7d0945c0b6eaf0dadb31228
이백행 <dlqorgod@naver.com>
2023-10-19 13:16:55 +0900

    egov.batch.db-web

---
+++ egov.batch.db-web/src/main/java/egovframework/example/bat/database/SpringContainerHsqlManager.java
@@ -0,0 +1,43 @@
+package egovframework.example.bat.database;
+
+import javax.annotation.PostConstruct;
+import javax.sql.DataSource;
+
+import org.hsqldb.util.DatabaseManagerSwing;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+
+@Repository("hsqlManager")
+public class SpringContainerHsqlManager {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(SpringContainerHsqlManager.class);
+	
+	@Autowired
+	DataSource dataSource;
+	
+	@Bean
+	public JdbcTemplate getJdbcTemplate(){
+	  return new JdbcTemplate(dataSource);
+	}
+	
+	@PostConstruct
+	public void init() {
+
+		//hsqldb
+		DatabaseManagerSwing.main(new String[] { "--url", "jdbc:hsqldb:mem:dataSource-hsql", "--user", "sa", "--password", "" });
+		LOGGER.debug("SpringContainerHsqlManager Start run ---------- ");
+
+		//derby
+		//DatabaseManagerSwing.main(new String[] { "--url", "jdbc:derby:memory:dataSource", "--user", "", "--password", "" });
+
+		//h2
+		//DatabaseManagerSwing.main(new String[] { "--url", "jdbc:h2:mem:dataSource", "--user", "sa", "--password", "" });
+
+	}
+
+}
