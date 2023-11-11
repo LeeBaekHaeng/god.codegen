bc09499e401cd921b7c3c2a01f2f92e563ac22cf
이백행 <dlqorgod@naver.com>
2023-11-11 09:01:47 +0900

    EgovFileExtensionException

--- god.com-all/src/main/java/god/test/cmm/web/TestCmmController.java
+++ god.com-all/src/main/java/god/test/cmm/web/TestCmmController.java
@@ -1,16 +1,30 @@
 package god.test.cmm.web;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import egovframework.com.cmm.ComDefaultVO;
+import egovframework.com.cmm.EgovMessageSource;
+import egovframework.com.cmm.exception.EgovFileExtensionException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Controller
+@RequiredArgsConstructor
+@Slf4j
 public class TestCmmController {
+
+    private final EgovMessageSource egovMessageSource;
 
     @GetMapping("/do/test/cmm/selectTestCmmList.do")
     public String selectTestCmmList(final ComDefaultVO comDefaultVO, final ModelMap model) {
@@ -19,6 +33,22 @@ public class TestCmmController {
             comDefaultVO.setSearchKeyword("test 이백행 " + LocalDateTime.now() + " 검색어");
 //            comDefaultVO.setSearchUseYn("Y");
             model.addAttribute("result", "test 이백행 " + LocalDateTime.now());
+        } else if ("Y".equals(comDefaultVO.getSearchUseYn())) {
+            final ObjectMapper mapper = new ObjectMapper();
+            mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+            List<ComDefaultVO> results = null;
+            try {
+                results = mapper.readValue("", new TypeReference<List<ComDefaultVO>>() {
+                });
+            } catch (JsonProcessingException e) {
+//                e.printStackTrace();
+                log.error("1-1 JsonProcessingException readValue");
+//                throw new BaseRuntimeException("1-2 JsonProcessingException readValue");
+//                throw new BaseRuntimeException("1-2 JsonProcessingException readValue");
+//                throw new BaseRuntimeException(egovMessageSource, "1-2 JsonProcessingException readValue");
+                throw new EgovFileExtensionException("1-2 JsonProcessingException readValue", "fail.common.msg");
+            }
+            log.debug("results={}", results);
         }
         return "god/test/cmm/TestCmmList";
     }
