ee51c6a30ee8bf67dee2bc4af47dce002ef79991
이백행 <dlqorgod@naver.com>
2024-01-04 09:44:26 +0900

    EgovPropertiesTest

---
+++ god.com/src/test/java/egovframework/com/cmm/service/EgovPropertiesTest.java
@@ -0,0 +1,14 @@
+package egovframework.com.cmm.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EgovPropertiesTest {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
