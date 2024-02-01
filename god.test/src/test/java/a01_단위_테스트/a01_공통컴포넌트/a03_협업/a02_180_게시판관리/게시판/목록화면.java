41a50ad8c9402d4b3e8d8607358a1a27d9d5cca8
이백행 <dlqorgod@naver.com>
2024-02-02 08:35:33 +0900

    [기능추가][셀레늄단위테스트]게시판 목록화면: assertEquals

--- god.test/src/test/java/a01_단위_테스트/a01_공통컴포넌트/a03_협업/a02_180_게시판관리/게시판/목록화면.java
+++ god.test/src/test/java/a01_단위_테스트/a01_공통컴포넌트/a03_협업/a02_180_게시판관리/게시판/목록화면.java
@@ -1,6 +1,10 @@
 package a01_단위_테스트.a01_공통컴포넌트.a03_협업.a02_180_게시판관리.게시판;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 import god.test.셀레늄_테스트;
 import lombok.NoArgsConstructor;
@@ -28,11 +32,21 @@ class 목록화면 extends 셀레늄_테스트 {
 		로그인("USER");
 
 		목록_화면이동();
+
+		assert__확인();
 	}
 
 	private void 목록_화면이동() {
 		driver.get(GET_URL + "/cop/bbs/selectArticleList.do?bbsId=BBSMSTR_000000000631jtlmrVtuCh");
 		정지();
+	}
+
+	private void assert__확인() {
+		final WebElement element = driver.findElement(By.cssSelector(".board h1"));
+		final String text = element.getText();
+		정지();
+
+		assertEquals("게시글 목록 (test 이백행 게시판명 2024-01-18T08:04:57.204780200)", text, "타이틀");
 	}
 
 }
