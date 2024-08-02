8eaa6f2f6516f0c7fd491b9ccdf7c2df70d2a982
이백행 <dlqorgod@naver.com>
2024-08-02 17:43:56 +0900

    no message

--- god.data/src/main/java/god/data/a/a/a/web/DataAaaRestController.java
+++ god.data/src/main/java/god/data/a/a/a/web/DataAaaRestController.java
@@ -1,5 +1,8 @@
 package god.data.a.a.a.web;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 import org.egovframe.rte.ptl.mvc.tags.ui.pagination.PaginationInfo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +11,7 @@ import egovframework.com.cmm.web.EgovComAbstractController;
 import god.data.a.a.a.service.DataAaaRequestVO;
 import god.data.a.a.a.service.DataAaaService;
 import god.data.cmm.service.DataResponseVO;
+import god.data.cmm.service.DataResponseVO.ComMsgHeader;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -41,14 +45,23 @@ public class DataAaaRestController extends EgovComAbstractController {
 			log.debug("dataAaaRequestVO={}", dataAaaRequestVO);
 		}
 
-		DataResponseVO<DataAaaResponseMsgHeader, DataAaaResponseMsgBody> dataResponseVO = new DataResponseVO<>();
+		final DataResponseVO<DataAaaResponseMsgHeader, DataAaaResponseMsgBody> dataResponseVO = new DataResponseVO<>();
+
+		// ComMsgHeader 공유서비스 공통 메시지헤더
+		final ComMsgHeader comMsgHeader = new ComMsgHeader();
+		comMsgHeader.setResponseTime(LocalDateTime.now().toString());
+		comMsgHeader.setResponseMsgID(UUID.randomUUID().toString());
+		comMsgHeader.setSuccessYN("Y");
+		comMsgHeader.setReturnCode("00");
+		dataResponseVO.setComMsgHeader(comMsgHeader);
 
 		final PaginationInfo paginationInfo = builderPaginationInfo(dataAaaRequestVO);
 		if (log.isDebugEnabled()) {
 			log.debug("paginationInfo={}", paginationInfo);
 		}
 
-		DataAaaResponseMsgBody msgBody = new DataAaaResponseMsgBody();
+		// MsgBody 서비스별 개별 메시지바디
+		final DataAaaResponseMsgBody msgBody = new DataAaaResponseMsgBody();
 		msgBody.setResults(dataAaaService.selectList(dataAaaRequestVO));
 		msgBody.setPaginationInfo(paginationInfo);
 		dataResponseVO.setMsgBody(msgBody);
